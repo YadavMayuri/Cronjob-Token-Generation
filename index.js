37224532e8c374e2412571fbc6bfc3f87ae89c4f
madhuri yadav <mayuriyadav54@gmail.com>
2023-06-28 12:10:36 +0530

    updates

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import router  from "./routes/AllRoutes.js";
+import dotenv from 'dotenv';
+import path from 'path';
 
 
 const app = express();
+
+dotenv.config();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+
+const __dirname = path.resolve();
+
+app.use((err, req, res, next) => {
+    console.log("Inside error middleware!!!")
+    if (err) {
+        return res.send(err)
+    } else {
+        next();
+    }
+})
+
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1',router);
@@ -18,10 +38,35 @@ app.use('/api/v1',router);
 // })
 // job.start();
 
-mongoose.connect('mongodb+srv://mayuriyadav54:HGU1ZbJCNcqlTu0z@cluster0.s9gcceb.mongodb.net/Cronjob-DB')
-.then(()=> console.log("DB connected"))
-.catch((err)=> console.log("DB Erroe=>",err))
+
+app.get("/ping", (req, res) => {
+    return res.sendFile(__dirname + '/public/index.html');
+})
 
 
-app.listen(8000, ()=> console.log("workin on port 8000"));
+app.get("/urlencoded", (req, res) => {
+    res.send(
+        `<form method='post' action='/login'>
+            <input name="email" placeholder="text" />
+            <input name="password"  placeholder="password"/>
+            <input type='submit' value="LOgin"/>
+        </form>`
+    )
+})
 
+app.post('/login', (req, res) => {
+    console.log(req.body.email)
+    console.log(req.body.password)
+    // db store 
+    res.send(`Your email ${req.body.email} and passsword is ${req.body.password}`)
+})
+
+
+
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB Error => ", err));
+
+
+app.listen(process.env.PORT, () => console.log("Working on port 8000")); // port
--- routes/AllRoutes.js
+++ routes/AllRoutes.js
@@ -1,27 +1,18 @@
 import express from "express";
-import { GetLocales, Market, getCurrencies } from "../controllers/CultureAPIControllers.js";
-import { Carriers } from "../controllers/CarriersAPIControllers.js";
+
 import { GetLocations } from "../controllers/GeoAPIControllers.js";
 import { CreateASearch, PollASearch, PollSearch } from "../controllers/FlightsLivePricesAPIControllers.js";
 import { CreateAndPollSearch } from "../controllers/SyncFlightLivePricesAPIControllers.js";
-import { PollAPageFromSearch, StartASearch } from "../controllers/HotelsLivePriceAPIControllers.js";
-import { autoSuggestFlights, autoSuggestHotels } from "../controllers/AutoSuggestAPIControllers.js";
+
 const router = express.Router();
 
 
-router.get('/GetLocales',GetLocales)
-router.get('/getCurrencies',getCurrencies)
-router.get('/Market',Market)
-router.get('/Carriers',Carriers)
 router.get('/GetLocations',GetLocations)
 router.post('/CreateASearch',CreateASearch)
 router.post('/PollSearch',PollSearch)
 router.get('/PollASearch',PollASearch)
 router.post('/CreateAndPollSearch',CreateAndPollSearch)
-router.post('/StartASearch',StartASearch)
-router.get('/PollAPageFromSearch',PollAPageFromSearch)
-router.get('/autoSuggestFlights',autoSuggestFlights)
-router.get('/autoSuggestHotels',autoSuggestHotels)
+
 
 
 
